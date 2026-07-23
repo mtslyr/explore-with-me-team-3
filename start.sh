7e4488b068c01a1896cdb4cd96272c30eacfda6e
Mikhail Tsilyurik <mtsilyurik@yandex.ru>
2026-07-23 20:48:37 +0500

    Subscription

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+echo "=== Очистка Docker ==="
+docker-compose down -v --remove-orphans
+
 echo "=== Сборка Maven ==="
-mvn clean package
+mvn clean package -DskipTests
 
 echo "=== Сборка Docker Compose ==="
 docker compose build --no-cache
