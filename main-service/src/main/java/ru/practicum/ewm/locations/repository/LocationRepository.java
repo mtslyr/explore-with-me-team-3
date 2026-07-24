375db46243896868d6b0ff3c5aa153bde32588b8
waywatcher847 <gmedved847@gmail.com>
2026-07-24 18:12:08 +0300

    postman

--- main-service/src/main/java/ru/practicum/ewm/locations/repository/LocationRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/locations/repository/LocationRepository.java
@@ -15,12 +15,12 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
     List<Location> findAllWithOffset(Pageable pageable);
 
     @Query(value = """
-            SELECT e.* FROM events e
+            SELECT e.event_id FROM events e
             JOIN locations l ON l.event_id = e.event_id
             WHERE distance(l.lat, l.lon, :lat, :lon) <= :radius
             ORDER BY e.event_date
             """, nativeQuery = true)
-    List<Event> findEventsWithinRadius(@Param("lat") Float lat,
-                                       @Param("lon") Float lon,
-                                       @Param("radius") Float radius);
+    List<Long> findEventIdsWithinRadius(@Param("lat") Float lat,
+                                        @Param("lon") Float lon,
+                                        @Param("radius") Float radius);
 }
