cbfc16d1c0da5a8e098677b6580c526a271313fe
waywatcher847 <gmedved847@gmail.com>
2026-07-24 08:48:10 +0300

    Update LocationNotFoundException.java

--- main-service/src/main/java/ru/practicum/ewm/common/exception/LocationNotFoundException.java
+++ main-service/src/main/java/ru/practicum/ewm/common/exception/LocationNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.practicum.ewm.common.exception;
 
-public class LocationNotFoundException extends RuntimeException {
+public class LocationNotFoundException extends NotFoundException {
     public LocationNotFoundException(Long id) {
         super("Location with id=" + id + " was not found");
     }
