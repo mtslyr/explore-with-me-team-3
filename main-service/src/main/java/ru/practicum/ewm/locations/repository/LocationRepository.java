0a43058ec535e5a56555a0738ac8b151795be407
waywatcher847 <gmedved847@gmail.com>
2026-07-24 18:20:58 +0300

    Update LocationRepository.java

--- main-service/src/main/java/ru/practicum/ewm/locations/repository/LocationRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/locations/repository/LocationRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.practicum.ewm.event.model.Event;
 import ru.practicum.ewm.locations.models.Location;
 
 import java.util.List;
